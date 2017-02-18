2b700e70f9d04b1c29825429bf4af827fbb76363
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2017-02-18 13:44:16 +0100

    version stable
    version stable

--- __manifest__.py
+++ __manifest__.py
@@ -13,9 +13,12 @@ Main Features
 """,
     'depends': [
         'sale',
-
+        'partner_wkf',
     ],
     'data': [
+       # 'report/layouts.xml',
+        'report/report.xml',
+        'report/bordereau.xml',
        'views/sale_order_view.xml',
        'views/account_invoice_view.xml',
        'report/sale_order_report_template.xml',
--- models/business.py
+++ models/business.py
@@ -11,15 +11,25 @@ from odoo.exceptions import UserError,ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    business_id = fields.Char(string="N° Dossier", size=12, copy=False,index=True, readonly=False)
+    business_id = fields.Char(string="N° Dossier", size=256, copy=False,index=True, readonly=False)
     site_id = fields.Text(string="Chantier", required=False)
+    period = fields.Char(string="Période",copy=False)
+    client_order_ref = fields.Char(string="Customer Reference",related='partner_id.code', store=True, readonly=True, copy=False)
 
-    @api.multi
-    @api.constrains('business_id',)
-    def _check_business_id_len(self):
-        for res in self:
-            if res.business_id and  len(res.business_id)!=12 :
-                  raise ValidationError(_('Le numéro du dossier doit avoir 12 positions \n. N° dossier saisi %s') % res.business_id)
+
+
+    # @api.onchange('partner_id')
+    # def onchange_customer(self):
+    #     if self.partner_id.code:
+    #         self.client_order_ref = self.partner_id.code
+
+
+    # @api.multi
+    # @api.constrains('business_id','state')
+    # def _check_business_id_len(self):
+    #     for res in self:
+    #         if res.state=='sale' and res.business_id and  len(res.business_id)!=12 :
+    #               raise ValidationError(_('Le numéro du dossier doit avoir 12 positions \n. N° dossier saisi %s') % res.business_id)
 
 
 
@@ -28,25 +38,32 @@ SaleOrder()
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    business_id = fields.Char(string="N° Dossier", size=12, copy=False, readonly=False)
+    business_id = fields.Char(string="N° Dossier", size=256, copy=False, readonly=False)
     site_id = fields.Text(string="Chantier", required=False,copy=False)
     period = fields.Char(string="Période",copy=False)
+    code_customer = fields.Char(string="Numéro client")
+    ref_attachment = fields.Char(string="Attachement N°")
+    nbr_exemplaire = fields.Char(string="Nombre d'exemplaire")
+    name = fields.Char(string='Référence', index=True,
+        readonly=False, copy=False, help='The name that will be used on account move lines')
 
-    @api.multi
-    @api.constrains('business_id',)
-    def _check_business_id_len(self):
-        for res in self:
-            if len(res.business_id)!=12 :
-                raise ValidationError(_('Le numéro du dossier doit avoir 12 positions \n. N° dossier saisi %s') % res.business_id)
+    # @api.multi
+    # @api.constrains('business_id',)
+    # def _check_business_id_len(self):
+    #     for res in self:
+    #         if res.business_id and len(res.business_id)!=12 :
+    #             raise ValidationError(_('Le numéro du dossier doit avoir 12 positions \n. N° dossier saisi %s') % res.business_id)
 
     @api.model
     def create(self, vals):
-        #raise ValidationError(_(self._context))
         if self._context.get('active_model',False)=='sale.order' :
             sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
             for order in sale_orders :
                vals['business_id']=order.business_id
                vals['site_id']=order.site_id
+               vals['period']=order.period
+               vals['code_customer']=order.client_order_ref
+               vals['name']=""
         return super(AccountInvoice, self.with_context(mail_create_nolog=True)).create(vals)
 
 AccountInvoice()
